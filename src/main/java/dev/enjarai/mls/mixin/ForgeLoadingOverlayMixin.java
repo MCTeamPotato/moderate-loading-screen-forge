eb01e0d8fe131d13fe77e6e4693ec412c14494bd
KL_jiana <2436262516@qq.com>
2025-01-12 14:16:24 +0800

    📷icon更新，🖊archivesBaseName修改

--- src/main/java/dev/enjarai/mls/mixin/ForgeLoadingOverlayMixin.java
+++ src/main/java/dev/enjarai/mls/mixin/ForgeLoadingOverlayMixin.java
@@ -43,14 +43,12 @@ public abstract class ForgeLoadingOverlayMixin extends Overlay {
         };
     }
 
-    // 背景颜色变量填充
     @ModifyVariable(method = "render", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraftforge/fml/earlydisplay/ColourScheme;background()Lnet/minecraftforge/fml/earlydisplay/ColourScheme$Colour;"))
     private ColourScheme.Colour moderateLoadingScreen$changeColorGl(ColourScheme.Colour constant) {
         Color color = Color.getColor("logo", ModConfig.backgroundColor.get());
         return new ColourScheme.Colour(color.getRed(), color.getGreen(), color.getBlue());
     }
 
-    // logo前注入图标
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;enableBlend()V", ordinal = 1, shift = At.Shift.BEFORE))
     private void moderateLoadingScreen$renderPatches(GuiGraphics graphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
         long k = Util.getMillis();
@@ -58,19 +56,16 @@ public abstract class ForgeLoadingOverlayMixin extends Overlay {
         moderateLoadingScreen$loadingScreen.renderPatches(new DrawContextWrapper(graphics), partialTick, f >= 1.0f);
     }
 
-    // 渲染后重置透明度
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;defaultBlendFunc()V"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void moderateLoadingScreen$resetTransparency(net.minecraft.client.gui.GuiGraphics context, int mouseX, int mouseY, float delta, CallbackInfo ci,
                                                          int i, int j, long l, float f, float g, float h) {
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, h);
     }
-    // logo透明度
     @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/VertexConsumer;color(FFFF)Lcom/mojang/blaze3d/vertex/VertexConsumer;", ordinal = 0), index = 3)
     private float moderateLoadingScreen$modifyLogoTransparency(float fade) {
         return fade * ModConfig.logoOpacity.get() / 100f;
     }
 
-    // 加载条透明度修改
     @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderColor(FFFF)V", ordinal = 0), index = 5)
     private float moderateLoadingScreen$modifyBarTransparency(float original) {
         return original * ModConfig.barOpacity.get() / 100f;
